53dc0f993fe06dfe74454f0f15aaf0e5d32f25d3
Sebastian Smerd <sebastiansmerd@SERVICES-MBP.station>
2024-02-26 18:03:10 +0000

    Added himepage content

--- creative_design/settings.py
+++ creative_design/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
+
 ]
 
 SITE_ID = 1
@@ -61,7 +63,10 @@ ROOT_URLCONF = 'creative_design.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "templates", "allauth")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
