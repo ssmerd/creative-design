0e646b30eb1ede79965e39e081297cbe926cf7d0
Sebastian Smerd <sebastiansmerd@SERVICES-MBP.station>
2024-03-02 22:38:10 +0000

    Build a collapsible menu main page header

--- creative_design/settings.py
+++ creative_design/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'designs',
 
 ]
 
@@ -152,6 +153,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
