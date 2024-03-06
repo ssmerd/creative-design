018f3e2dfc19a0874d0df2369e40a037cbdaa161
Sebastian Smerd <sebastiansmerd@SERVICES-MBP.station>
2024-03-06 16:08:12 +0000

    Added designs views and templates

--- designs/views.py
+++ designs/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .models import Design
 
 def all_designs(request):
-    pass
+    """
+        A view to show all designs
+    """
+
+    designs = Design.objects.all()
+
+    context = {
+        'designs': designs,
+    }
+
+    return render(request ,"designs/designs.html", context)
