ee1f316610f386031ef64ac9dc3fff64b9672ff7
Sebastian Smerd <sebastiansmerd@SERVICES-MBP.station>
2024-03-06 15:54:02 +0000

    Customized admin

--- designs/admin.py
+++ designs/admin.py
@@ -3,5 +3,18 @@ from .models import Category, Design
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Design)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class DesignAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'image',
+    )
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Design, DesignAdmin)
