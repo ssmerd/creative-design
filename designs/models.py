108fe2fb7872cd3560156dc758244eeecdb70ce2
Sebastian Smerd <sebastiansmerd@SERVICES-MBP.station>
2024-03-06 15:46:02 +0000

    Added models and fixtures in design app

--- designs/models.py
+++ designs/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Design(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
