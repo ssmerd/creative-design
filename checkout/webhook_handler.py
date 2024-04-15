2e9ec8d2264a37d35c2ca5822975cbb4871e8d0a
ssmerd <ssmerd@gmail.com>
2024-04-15 19:14:16 +0000

    Webhook updated

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -4,8 +4,7 @@ from django.template.loader import render_to_string
 from django.conf import settings
 
 from .models import Order
-# from products.models import Product
-# from profiles.models import UserProfile
+from profiles.models import UserProfile
 
 import json
 import time
@@ -69,25 +68,15 @@ class StripeWH_Handler:
         size = quote["size"]
         total = round(stripe_charge.amount / 100, 2) 
 
-        # Clean data in the shipping details
-        # for field, value in shipping_details.address.items():
-        #     if value == "":
-        #         shipping_details.address[field] = None
-
+       
         # Update profile information if save_info was checked
-        # profile = None
-        # username = intent.metadata.username
-        # if username != 'AnonymousUser':
-        #     profile = UserProfile.objects.get(user__username=username)
-        #     if save_info:
-        #         profile.default_phone_number = shipping_details.phone
-        #         profile.default_country = shipping_details.address.country
-        #         profile.default_postcode = shipping_details.address.postal_code
-        #         profile.default_town_or_city = shipping_details.address.city
-        #         profile.default_street_address1 = shipping_details.address.line1
-        #         profile.default_street_address2 = shipping_details.address.line2
-        #         profile.default_county = shipping_details.address.state
-        #         profile.save()
+        profile = None
+        if name != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=name)
+            if save_info:
+                profile.default_phone = billing_details.phone
+                profile.default_email = billing_details.email
+                profile.save()
 
         order_exists = False
         attempt = 1
@@ -119,7 +108,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     name=name,
-                    # user_profile=profile,
+                    user_profile=profile,
                     email=email,
                     phone=phone,
                     category=category,
--- profiles/views.py
+++ profiles/views.py
@@ -23,13 +23,13 @@ def profile(request):
                             'the form is valid.'))
     else:
         form = UserProfileForm(instance=profile)
+        
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
-        'on_profile_page': True
     }
 
     return render(request, template, context)
@@ -46,7 +46,6 @@ def order_history(request, order_number):
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
-        'from_profile': True,
     }
 
     return render(request, template, context)
