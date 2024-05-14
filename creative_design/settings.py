d130ff6e10e3db342e461a3294667046782bd41a
ssmerd <ssmerd@gmail.com>
2024-05-14 19:02:15 +0000

    Deployment

--- creative_design/settings.py
+++ creative_design/settings.py
@@ -21,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dlp@@i(9=nz8$*uxme$+lzxt!k$+s@xx!so^q8d2j!y_u)!=v6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 # ALLOWED_HOSTS = ['8000-ssmerd-creative-design-x7k6aq2t1k.us1.codeanyapp.com']
 
